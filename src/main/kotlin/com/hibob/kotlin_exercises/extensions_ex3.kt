82f9942ab1a8c59325b52a9e9284e917c97e0a35
Israel Cohen <israel.cohen@hibob.io>
2024-09-03 12:02:16 +0300

    commit ex3 - extension

---
+++ src/main/kotlin/com/hibob/kotlin_exercises/extensions_ex3.kt
@@ -0,0 +1,34 @@
+package com.hibob.kotlin_exercises
+
+import kotlin.math.pow
+
+// q1:
+//fun sum(list: List<Int>):Int {
+//    var result = 0
+//    for (i in list) {
+//        result += i
+//    }
+//    return result
+//}
+
+// Ans_1:
+fun List<Int>.sum() : Int{
+    var sum = 0
+    this.forEach { item ->
+        sum += item
+    }
+    return sum
+}
+
+//import kotlin.math.pow
+infix fun Number.toPowerOf(exponent: Number): Double {
+    return this.toDouble().pow(exponent.toDouble())
+}
+
+fun main() {
+    val numList = listOf(1,2,3,4,5)
+
+    println(numList.sum())
+    println(3.toPowerOf(2))
+    println(2.toPowerOf(0))
+}
