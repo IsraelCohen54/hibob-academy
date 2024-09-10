6b75d322ef5bd969ddbd10a13220a8f70aeb6382
Israel Cohen <israel.cohen@hibob.io>
2024-09-10 11:17:47 +0300

    ex9 Israel.C updated files according to the feedback

--- src/main/resources/db/migration/V202409041445__pets.sql
+++ src/main/resources/db/migration/V202409041445__pets.sql
@@ -1,5 +1,3 @@
--- V1__create_pets_table.sql
-
 CREATE TABLE pets (
                       id SERIAL primary key,
                       name VARCHAR(30) NOT NULL,
@@ -10,10 +8,13 @@ CREATE TABLE pets (
 
 CREATE INDEX idx_company_id ON pets (company_id);
 
-INSERT INTO public.pets (name, type, company_id, date_of_arrival)
+/*
+INSERT INTO pets (name, type, company_id, date_of_arrival)
 VALUES ('Witi', 'Dog', 1, '2020-01-28');
+
 INSERT INTO public.pets (name, type, company_id, date_of_arrival)
 VALUES ('Piti', 'Cat', 1, '2016-01-28');
+
 INSERT INTO public.pets (name, type, company_id, date_of_arrival)
 VALUES ('Bobi', 'Dog', 2, '2023-02-24');
 
@@ -27,4 +28,4 @@ WHERE id = 1;
 SELECT *
 FROM pets
 WHERE date_of_arrival < CURRENT_DATE - INTERVAL '1 year';
-
+*/
--- src/main/resources/db/migration/V202409041458__owner.sql
+++ src/main/resources/db/migration/V202409041458__owner.sql
@@ -1,9 +1,15 @@
--- V1__create_owner_table.sql
-
 CREATE TABLE owner (
+                       id SERIAL primary key,
                        name VARCHAR(30) NOT NULL,
                        company_id INT NOT NULL,
                        employee_id INT NOT NULL
 );
 
 CREATE INDEX idx_company_employee ON owner (company_id, employee_id);
+/*
+INSERT INTO owner (name, company_id, employee_id)
+VALUES ('Josh', '9', 123123123);
+
+INSERT INTO owner (name, company_id, employee_id)
+VALUES ('Ariel', '9', 999123123);
+*/
