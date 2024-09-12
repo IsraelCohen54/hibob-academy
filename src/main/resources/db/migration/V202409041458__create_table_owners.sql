3304a66a2acd77d1a86652616f82aa23b1f8c417
Israel Cohen <israel.cohen@hibob.io>
2024-09-12 09:13:37 +0300

    master fix owner migration pr

--- src/main/resources/db/migration/V202409041458__create_table_owners.sql
+++ src/main/resources/db/migration/V202409041458__create_table_owners.sql
@@ -1,16 +1,16 @@
-CREATE TABLE owner (
+CREATE TABLE owners (
                        id SERIAL primary key,
                        name VARCHAR(30) NOT NULL,
                        company_id BIGINT NOT NULL,
-                       employee_id VARCHAR NOT NULL,
+                       employee_id VARCHAR NOT NULL
 );
 
-CREATE INDEX idx_owner_company_employee ON owner (company_id, employee_id);
+CREATE unique INDEX idx_owner_company_employee ON owners (company_id, employee_id);
 
-
-INSERT INTO owner (name, company_id, employee_id)
+/*
+INSERT INTO owners (name, company_id, employee_id)
 VALUES ('Josh', '9', 123123123);
 
-INSERT INTO owner (name, company_id, employee_id)
+INSERT INTO owners (name, company_id, employee_id)
 VALUES ('Ariel', '9', 999123123);
-
+*/
