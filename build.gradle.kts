79fdcfb39edef5539ee319b570580d808712ff61
Israel <73114054+IsraelCohen54@users.noreply.github.com>
2024-09-16 11:09:53 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.flywaydb.flyway") version "10.4.1"
+}
+
+buildscript {
+	dependencies {
+		classpath("org.flywaydb:flyway-database-postgresql:10.4.1")
+	}
 }
 
 java {
@@ -16,6 +23,11 @@ java {
 
 repositories {
 	mavenCentral()
+}
+
+flyway {
+	url = "jdbc:postgresql://localhost:5432/academy?user=bob&password=dev"
+	driver = "org.postgresql.Driver"
 }
 
 dependencies {
