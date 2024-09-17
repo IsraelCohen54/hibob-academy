6f552c0d5176ebf3eff52634b3fa2d5e6eda555f
Israel <73114054+IsraelCohen54@users.noreply.github.com>
2024-09-17 18:07:58 +0300

    ex12 Israel.C controller + authentication (#12)
    * ex12 Israel.C controller + authentication
    
    * israel.c/ resolved all
    
    * israel.c/ updated
    
    * israel.c/ updated
    
    * israel.c/ updated
    
    ---------
    
    Co-authored-by: Israel Cohen <israel.cohen@hibob.io>

---
+++ src/main/kotlin/com/hibob/academy/filter/AuthenticationFilter.kt
@@ -0,0 +1,38 @@
+package com.hibob.academy.filter
+
+import com.hibob.academy.service.SessionService.Companion.SECRET_KEY
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.container.ContainerRequestFilter
+import jakarta.ws.rs.ext.Provider
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+const val CREATE_TOKEN_PATH = "createToken"
+const val COOKIE_NAME = "JWT"
+
+@Component
+@Provider
+class AuthenticationFilter: ContainerRequestFilter {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun filter(requestContext: ContainerRequestContext) {
+
+        if (requestContext.uriInfo.path != CREATE_TOKEN_PATH) {
+            verify(requestContext.cookies[COOKIE_NAME]?.value)
+            logger.info("Authorized successfully")
+        }
+    }
+
+    fun verify(cookie: String?): Jws<Claims> {
+        cookie?.let {
+            return try {
+                Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(it)
+            } catch (e: Exception) {
+                throw Exception("unauthorized error: ${e.message}")
+            }
+        } ?: throw Exception("unauthorized error")
+    }
+}
