5c90014ec7470e85fb65d2f3fbda07f9416cf6d9
Israel Cohen <israel.cohen@hibob.io>
2024-09-17 17:01:37 +0300

    israel.c/ updated

--- src/main/kotlin/com/hibob/academy/filter/AuthenticationFilter.kt
+++ src/main/kotlin/com/hibob/academy/filter/AuthenticationFilter.kt
@@ -4,13 +4,11 @@ import com.hibob.academy.service.SessionService.Companion.SECRET_KEY
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
-import jakarta.ws.rs.container.ContainerRequestFilter
 import jakarta.ws.rs.container.ContainerRequestContext
-import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.container.ContainerRequestFilter
 import jakarta.ws.rs.ext.Provider
-import org.springframework.stereotype.Component
 import org.slf4j.LoggerFactory
-
+import org.springframework.stereotype.Component
 
 const val CREATE_TOKEN_PATH = "createToken"
 const val COOKIE_NAME = "JWT"
@@ -22,30 +20,19 @@ class AuthenticationFilter: ContainerRequestFilter {
 
     override fun filter(requestContext: ContainerRequestContext) {
 
-
         if (requestContext.uriInfo.path == CREATE_TOKEN_PATH) return
 
-        try {
-            val verificationResult = verify(requestContext.cookies[COOKIE_NAME]?.value)
-            if (verificationResult == null) {
-                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build())
-            } else {
-                logger.info("Authorized successfully")
-            }
-        } catch (e: Exception) {
-            logger.error("Error during verification", e)
-            requestContext.abortWith(Response.status(Response.Status.INTERNAL_SERVER_ERROR).build())
-        }
+        verify(requestContext.cookies[COOKIE_NAME]?.value)
+        logger.info("Authorized successfully")
     }
 
-    fun verify(cookie: String?): Jws<Claims>? {
+    fun verify(cookie: String?): Jws<Claims> {
         cookie?.let {
-            try {
+            return try {
                 Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(it)
             } catch (e: Exception) {
-                return null
+                throw Exception("unauthorized error: ${e.message}")
             }
-        }
-        return null
+        } ?: throw Exception("unauthorized error")
     }
 }
