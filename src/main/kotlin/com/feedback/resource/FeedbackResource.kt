db38489f14e51ce38aedcbd17c589fc680abf97a
Israel Cohen <israel.cohen@hibob.io>
2024-09-26 01:10:32 +0300

    israel.c/ I would like to get some feedback here... even without merge.

---
+++ src/main/kotlin/com/feedback/resource/FeedbackResource.kt
@@ -0,0 +1,52 @@
+package com.feedback.resource
+
+import com.feedback.dao.FeedbackCreationRequest
+import com.feedback.dao.LoggedInUser
+import com.feedback.service.EmployeeFetcher
+import com.feedback.service.FeedbackInserter
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.Cookie
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.core.Response.Status
+import org.springframework.stereotype.Component
+
+@Component
+@Path("/api/feedback")
+@Produces("application/json")
+@Consumes("application/json")
+class FeedbackResource(
+    private val feedbackInserter: FeedbackInserter,
+    private val employeeFetcher: EmployeeFetcher,
+    private val loginDetailsValidator: LoginDetailsValidator,
+    private val extractCookiesData: ExtractCookiesData
+) {
+
+    @POST
+    fun submitFeedback(
+        feedbackRequest: FeedbackRequest,
+        @Context cookies: Map<String, Cookie>
+    ): Response {
+        val loggedInUser = extractCookiesData.extractCompanyIdEmployeeId(cookies)
+
+        val department = fetchEmployeeDepartmentByAnonymity(loggedInUser, feedbackRequest.isAnonymous)
+        val feedbackCreationRequest = FeedbackCreationRequest(department, feedbackRequest.comment)
+
+        loginDetailsValidator.validateCompanyId(loggedInUser.companyId)
+        loginDetailsValidator.validateEmployeeId(loggedInUser.employeeId)
+
+        feedbackInserter.insertFeedback(loggedInUser, feedbackCreationRequest)
+
+        return Response.status(Status.OK).build()
+    }
+
+    private fun fetchEmployeeDepartmentByAnonymity(loggedInUser: LoggedInUser, isAnonymous: Boolean) =
+        if (isAnonymous) null else employeeFetcher.getEmployeeDetails(loggedInUser).department
+
+
+}
+
+data class FeedbackRequest(val comment: String, val isAnonymous: Boolean)
